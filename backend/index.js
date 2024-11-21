ff5908a3d8741fba2969a99a3b3af0d3137da60e
KanishkRAJ29 <114609861+KanishkRAJ29@users.noreply.github.com>
2024-11-21 14:44:25 +0530

    a

--- backend/index.js
+++ backend/index.js
@@ -18,5 +18,17 @@ app.use(express.urlencoded({extended: true}))
 app.use("/api/v1/message",messageRouter);
 app.use("/api/v1",checkoutRouter);
 
+
+app.get("/", (req, res) => {
+  res.send("Welcome to the API! Use the /api/v1/message or /api/v1 endpoints.");
+});
+
+app.use((req, res, next) => {
+  res.status(404).json({
+      success: false,
+      message: "The route you are trying to access does not exist on this server.",
+  });
+});
+
 dbConnections();
 export default app;
